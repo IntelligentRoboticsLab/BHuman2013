0f6ab6ec27953924b8137ad323f59e9a165b6316
Patrick de Kok <patrick@cxiu.nl>
2014-03-20 20:31:40 +0100

    ColorDiscretizer is cleaned up

--- Src/Tools/ColorDiscretizer.h
+++ Src/Tools/ColorDiscretizer.h
@@ -1,43 +1,31 @@
-#ifndef COLORDISCRETIZER_H
-#define COLORDISCRETIZER_H
+#pragma once
 
 #include <opencv2/core/core.hpp>
-#include "Tools/ImageProcessing/ImagePrimitives.h"
+
 #include "Representations/Infrastructure/Image.h"
-#include <fstream>
-#include <stdio.h>
-#include <string>
 
 class ColorDiscretizer
 {
-public:
-    ColorDiscretizer();
-    ~ColorDiscretizer();
-    /*
-    */
-    void setClusters(int num_clusters);
-    // TODO: fix the cluster number arguments
-    bool initializeColorModel(cv::InputArrayOfArrays images, int clusters=-1);
-    bool initializeColorModel(const std::vector<naoth::Image> &images, int clusters=-1);
-    bool initializeColorModel(const std::vector<Pixel> &pixels, int clusters=-1);
-    void discretize(cv::InputArray image, cv::OutputArray discretizedImage);
-    void discretize(const naoth::Image &image, std::vector<std::vector<int> > &discretizedImage);
-    void discretize(const std::vector<Pixel> &pixels, std::vector<int> &discretizedPixels);
-    inline int getClusters() const { return this->clusters; }
-    inline cv::Mat getClusterColors() const { return this->clusterColors; }
-    void saveClusters(const char* fileName);
-    void readClusters(const char* fileName);
-    unsigned int nearestNeighborIndex(float channel1, float channel2, float channel3);
-private:
-    unsigned int clusters;
-    cv::Mat clusterColors;
-    bool isClustersIndexed;
-    inline bool checkClusters(int clusters) { return (clusters > 0) && (this->clusters = clusters); }
-    void generateClusterIndex(const cv::Mat &samples,
-                              const cv::TermCriteria &criteria=cv::TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 5, 1.0),
-                              int attempts=3,
-                              int flags=cv::KMEANS_PP_CENTERS);
+    public:
+        ColorDiscretizer(unsigned clusterNum);
+        void setClusters(unsigned clusterNum);
+        bool initializeColorModel(const std::vector<Image::Pixel> & pixels);
+        std::vector<int> discretize(const std::vector<Image::Pixel> & pixels);
+        unsigned getColorClass(float channel1, float channel2, float channel3);
 
+        void saveClusters(const std::string & filename);
+        void readClusters(const std::string & filename);
+
+        inline int getClusterNum() const { return clusterNum_; }
+        inline cv::Mat getClusterColors() const { return clusterColors_; }
+
+    private:
+        unsigned clusterNum_;
+        cv::Mat clusterColors_;
+        bool isClustersIndexed_;
+        inline bool checkClusters(int clusterNum) { return (clusterNum > 0) && (clusterNum_ = clusterNum); }
+        void generateClusterIndex(const cv::Mat & samples,
+                                  const cv::TermCriteria & criteria=cv::TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 5, 1.0),
+                                  int attempts=3,
+                                  int flags=cv::KMEANS_PP_CENTERS);
 };
-
-#endif // COLORDISCRETIZER_H
